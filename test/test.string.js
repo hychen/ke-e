4d1adf81f112f26c7a9bf7254bc48201bd32e36a
陳信屹 <ossug.hychen@gmail.com>
2016-09-05 15:03:35 +0800

    Add String test.

---
+++ test/test.string.js
@@ -0,0 +1,14 @@
+describe('String', () => {
+
+  it('generate a random string.', () => {
+    let c0 = hc.char.generate();
+    expect(_.isString(c0)).eq(true);
+    let c1 = hc.asciichar.generate();
+    expect(_.isString(c1)).eq(true);
+    let s0 = hc.string.generate();
+    expect(_.isString(s0)).eq(true);
+    let s1 = hc.asciistring.generate();
+    expect(_.isString(s1)).eq(true);
+  });
+});
+
